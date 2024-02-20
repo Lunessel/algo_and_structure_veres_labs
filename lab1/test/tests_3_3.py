e35052652b96b3500ee0e156fb92de9b663a82b4
Lunessel <varloky@gmail.com>
2024-02-20 10:38:06 +0200

    solution to lab1 (longest peak)

---
+++ lab1/test/tests_3_3.py
@@ -0,0 +1,24 @@
+import unittest
+from lab1.src.lab1_3_3 import *
+
+
+class TestFindLongestPick(unittest.TestCase):
+    def test_given_array(self):
+        result = find_longest_peak([1, 3, 5, 4, 2, 8, 3, 7])
+        self.assertEqual(result, 5)
+
+    def test_sorted_array(self):
+        result = find_longest_peak([1, 2, 3, 4, 5, 6, 7])
+        self.assertEqual(result, -1)
+
+    def test_pick_with_double_same_value(self):
+        result = find_longest_peak([1, 2, 6, 6, 3])
+        self.assertEqual(result, -1)
+
+    def test_single_element_array(self):
+        result = find_longest_peak([2])
+        self.assertEqual(result, -1)
+
+
+if __name__ == "__main":
+    unittest.main()
